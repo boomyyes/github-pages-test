e434c69638ce80e608d73c052009d389ea0a3e60
Avanish Wankhede <avanishwankhede@gmail.com>
2025-10-27 22:10:54 +0530

    docs: Configure GitHub Pages deployment

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
 import './App.css';
+import { Component } from 'react'; 
+import { HashRouter, Route, Routes, Link } from "react-router-dom"; 
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+
+const Home = () => <div><h2>Home</h2></div> 
+const About = () => <div><h2>About</h2></div> 
+
+class App extends Component { 
+render() { 
+return ( 
+<HashRouter basename="/"> 
+<div> 
+<ul> 
+<li><Link to="/">Home</Link></li> 
+<li><Link to="/about">About</Link></li> 
+</ul> 
+<hr /> 
+<Routes>
+  <Route exact path="/" component={Home} /> 
+  <Route path="/about" component={About} /> 
+</Routes>
+</div> 
+</HashRouter> 
+); 
+} 
+} 
 
 export default App;
